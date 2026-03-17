6834e5ad7a0a3523a6b704ed62652f1bbd0fde9c
lucyheaney <l.heaney.1@research.gla.ac.uk>
2026-03-17 18:38:44 +0100

    changes to errors, running with acceptable relative error, GPU compoatible

--- weno5.jl
+++ weno5.jl
@@ -1,35 +1,45 @@
 # WENO5 FUNCTION
 
+struct dim{D} end
+
 function polys(fmm,fm,fc,fp,fpp)
-    p1 = (2*fmm .- 7*fm .+ 11*fc )/6 # polynomials
-    p2 = (-fm .+ 5*fc .+ 2*fp )/6
-    p3 = (2*fc .+ 5*fp .- fpp)/6
 
-    b1 = 13/12*(fmm .- 2*fm .+ fc ).^2 .+ 1/4*(fmm .- 4*fm .+ 3*fc ).^2 # smoothenss
-    b2 = 13/12*(fm  .- 2*fc .+ fp ).^2 .+ 1/4*(fm .- fp ).^2
-    b3 = 13/12*(fc  .- 2*fp .+ fpp).^2 .+ 1/4*(3*fc  .- 4*fp .+   fpp).^2
+    # Prefactors
+    a1 = 2f0
+    a2 = 5f0
+    a3 = 13f0/12f0
+    a4 = 1f0/4f0
 
-    g = (0.1, 0.6, 0.3)
-    eps = 1e-6
-    wp1 = g[1] ./ (b1 .+ eps)^2
-    wp2 = g[2] ./ (b2 .+ eps)^2
-    wp3 = g[3] ./ (b3 .+ eps)^2
+    # p1 = (2*fmm .- 7*fm .+ 11*fc )/6 # polynomials
+    p1 = (a1*fmm .- 7f0*fm .+ 11f0*fc )*inv(6f0) # polynomials
+    p2 = (-fm .+ a2*fc .+ a1*fp )*inv(6f0)
+    p3 = (a1*fc .+ a2*fp .- fpp)*inv(6f0)
+
+    b1 = a3*(fmm .- a1*fm .+ fc ).^2f0 .+ a4*(fmm .- 4f0*fm .+ 3f0*fc ).^2f0 # smoothenss
+    b2 = a3*(fm  .- a1*fc .+ fp ).^2f0 .+ a4*(fm .- fp ).^2f0
+    b3 = a3*(fc  .- a1*fp .+ fpp).^2f0 .+ a4*(3f0*fc .- 4f0*fp .+ fpp).^2f0
+
+    g = (0.1f0, 0.6f0, 0.3f0)
+    eps = 1f-6
+    wp1 = g[1] ./ (b1 .+ eps)^2f0
+    wp2 = g[2] ./ (b2 .+ eps)^2f0
+    wp3 = g[3] ./ (b3 .+ eps)^2f0
 
     flux_face = (wp1 .* p1 .+ wp2 .* p2 .+ wp3 .* p3) ./ (wp1 .+ wp2 .+ wp3)
     
     return flux_face
 end
 
-function stencil(f, dim, I, nx, ny)
+
+function stencil(f, ::dim{0}, I, nx, ny) # x direction
 
     i, j = Tuple(I)
         
-    boundary = (1,1,0,0)
+    boundary_x = (1,1) # left, right
 
-    left = Val(boundary[1])
-    right = Val(boundary[2])
-    bottom = Val(boundary[3])
-    top = Val(boundary[4])
+    left = Val(boundary_x[1])
+    right = Val(boundary_x[2])
+    
 
     illl = minus_index(i,3,nx, left)
     ill  = minus_index(i,2,nx, left)
@@ -38,6 +48,32 @@ function stencil(f, dim, I, nx, ny)
     irr  = plus_index(i,2,nx, right)
     irrr = plus_index(i,3,nx, right)
 
+    fmmm = f[illl, j]
+    fmm  = f[ill, j]
+    fm   = f[il, j]
+    fc   = f[i,   j]
+    fp   = f[ir, j]
+    fpp  = f[irr, j]
+    fppp = f[irrr, j]
+
+    fppos = polys(fmmm, fmm, fm, fc, fp) # need to define these stencils for the weno5 scheme - will need to be adapted to work with the grid structure and kernel launches
+    fpneg = polys(fppp, fpp, fp, fc, fm)
+    fmpos = polys(fmmm, fmm, fm, fc, fp)
+    fmneg = polys(fpp, fp, fc, fm, fmm)
+
+    return fppos, fpneg, fmpos, fmneg
+
+end
+
+function stencil(f, ::dim{1}, I, nx, ny) # y direction
+
+    i, j = Tuple(I)
+        
+    boundary_y = (0,0) # bottom, top
+
+    bottom = Val(boundary_y[1])
+    top = Val(boundary_y[2])
+
     jbbb = minus_index(j,3,ny, bottom)
     jbb  = minus_index(j,2,ny, bottom)
     jb   = minus_index(j,1,ny, bottom)
@@ -45,67 +81,46 @@ function stencil(f, dim, I, nx, ny)
     jtt  = plus_index(j,2,ny, top)
     jttt = plus_index(j,3,ny, top)
 
+    fmmm = f[i, jbbb]
+    fmm  = f[i, jbb]
+    fm   = f[i, jb]
+    fc   = f[i, j]
+    fp   = f[i, jt]
+    fpp  = f[i, jtt]
+    fppp = f[i, jttt]
 
-    if dim == "x"
-
-        fmmm = f[illl, j]
-        fmm  = f[ill, j]
-        fm   = f[il, j]
-        fc   = f[i,   j]
-        fp   = f[ir, j]
-        fpp  = f[irr, j]
-        fppp = f[irrr, j]
-
-        fppos = polys(fmmm, fmm, fm, fc, fp) # need to define these stencils for the weno5 scheme - will need to be adapted to work with the grid structure and kernel launches
-        fpneg = polys(fppp, fpp, fp, fc, fm)
-        fmpos = polys(fmmm, fmm, fm, fc, fp)
-        fmneg = polys(fpp, fp, fc, fm, fmm)
-
-    elseif dim == "y"
-        fmmm = f[i, jbbb]
-        fmm  = f[i, jbb]
-        fm   = f[i, jb]
-        fc   = f[i, j]
-        fp   = f[i, jt]
-        fpp  = f[i, jtt]
-        fppp = f[i, jttt]
-
-        fppos = polys(fmmm, fmm, fm, fc, fp) # need to define these stencils for the weno5 scheme - will need to be adapted to work with the grid structure and kernel launches
-        fpneg = polys(fppp, fpp, fp, fc, fm)
-        fmpos = polys(fmmm, fmm, fm, fc, fp)
-        fmneg = polys(fpp, fp, fc, fm, fmm)
-    end
+    fppos = polys(fmmm, fmm, fm, fc, fp) # need to define these stencils for the weno5 scheme - will need to be adapted to work with the grid structure and kernel launches
+    fpneg = polys(fppp, fpp, fp, fc, fm)
+    fmpos = polys(fmmm, fmm, fm, fc, fp)
+    fmneg = polys(fpp, fp, fc, fm, fmm)
 
     return fppos, fpneg, fmpos, fmneg
 
 end
 
-function face_velocity(V, dim, i, j)
+function face_velocity(V, ::dim{0}, i, j)
 
-    if dim == "x"
+    vxm = V.x[i-1,j]
+    vxp = V.x[i+1,j]
 
-        vx = V.x[i,j]
-        vxm = V.x[i-1,j]
-        vxp = V.x[i+1,j]
+    vmpos = 0.5f0 * (vxm + abs(vxm))
+    vmneg = 0.5f0 * (vxm - abs(vxm))
+    vppos = 0.5f0 * (vxp + abs(vxp))
+    vpneg = 0.5f0 * (vxp - abs(vxp))
 
-        vmpos = 0.5 * (vxm + abs(vxm))
-        vmneg = 0.5 * (vxm - abs(vxm))
+    return vmpos, vmneg, vppos, vpneg
 
-        vppos = 0.5 * (vxp + abs(vxp))
-        vpneg = 0.5 * (vxp - abs(vxp))
+end
 
-    elseif dim == "y"
+function face_velocity(V, ::dim{1}, i, j)
 
-        vy = V.y[i,j]
-        vym = V.y[i,j-1]
-        vyp = V.y[i,j+1]
+    vym = V.y[i,j-1]
+    vyp = V.y[i,j+1]
 
-        vmpos = 0.5 * (vym + abs(vym))
-        vmneg = 0.5 * (vym - abs(vym))
-
-        vppos = 0.5 * (vyp + abs(vyp)) 
-        vpneg = 0.5 * (vyp - abs(vyp))
-    end
+    vmpos = 0.5f0 * (vym + abs(vym))
+    vmneg = 0.5f0 * (vym - abs(vym))
+    vppos = 0.5f0 * (vyp + abs(vyp)) 
+    vpneg = 0.5f0 * (vyp - abs(vyp))
 
     return vmpos, vmneg, vppos, vpneg
 
@@ -115,11 +130,11 @@ function advection_fct!(q_adv, f, V, dx, nx, ny, g::StructuredGrid, I, O)
     
     i, j = Tuple(I)
 
-    vxmpos, vxmneg, vxppos, vxpneg = face_velocity(V, "x", i, j) # get the face velocities for the x direction - will need to be adapted to work with the grid structure and kernel launches vypmpos, vypmneg, vyppos, vypneg = face_velocity(V, "y") fxppos, fxpneg, fxmpos, fxmneg = stencil(f, "x", I) # get the stencils for the x direction - will need to be adapted to work with the grid structure and kernel launches fyppos, fypneg, fympos, fymneg = stencil(f, "y", I) # compute the WENO5 fluxes for x and y directions using the stencils and face velocities wp1 = g[1] ./ (b1 .+ eps)^2 wp2 = g[2] ./ (b2 .+ eps)^2
-    vympos, vymneg, vyppos, vypneg = face_velocity(V, "y", i, j) # get the face velocities for the y direction - will need to be adapted to work with the grid structure and kernel launches # compute the WENO5 fluxes for x and y directions using the stencils and face velocities F_xphalf = vxmpos * fxppos + vxmneg * fxpneg F_xmhalf = vxmpos * fxmpos + vxmneg * fxmneg q_adv.x[I...] = (F_xphalf - F_xmhalf) / dx F_yphalf = vympos * fyppos + vymneg * fypneg F_ymhalf = vympos * fympos + vymneg * fymneg q_adv.y[I...] = (F_yphalf - F_ymhalf) / dx end
+    vxmpos, vxmneg, vxppos, vxpneg = face_velocity(V, dim{0}(), i, j) # get the face velocities for the x direction - will need to be adapted to work with the grid structure and kernel launches
+    vympos, vymneg, vyppos, vypneg = face_velocity(V, dim{1}(), i, j) # get the face velocities for the y direction - will need to be adapted to work with the grid structure and kernel launches
 
-    fxppos, fxpneg, fxmpos, fxmneg = stencil(f, "x", I, nx, ny) # get the stencils for the x direction - will need to be adapted to work with the grid structure and kernel launches
-    fyppos, fypneg, fympos, fymneg = stencil(f, "y", I, nx, ny)
+    fxppos, fxpneg, fxmpos, fxmneg = stencil(f, dim{0}(), I, nx, ny) # get the stencils for the x direction - will need to be adapted to work with the grid structure and kernel launches
+    fyppos, fypneg, fympos, fymneg = stencil(f, dim{1}(), I, nx, ny)
     
 
     qxp = vxppos .* fxppos + vxpneg .* fxpneg
